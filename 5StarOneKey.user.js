e4c142bf84a79a9f276122ddc25db35fe4061c3a
XiFanU <31371729+xifanu@users.noreply.github.com>
2017-12-04 14:16:40 +0800

    Update 5StarOneKey.user.js

--- 5StarOneKey.user.js
+++ 5StarOneKey.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         5 Star One Key
-// @version      0.25
+// @version      0.25.1
 // @description  Give five star with single click
 // @updateURL    https://github.com/jqqqqqqqqqq/MakeOPRGreatAgain/raw/master/5StarOneKey.user.js
 // @downloadURL  https://github.com/jqqqqqqqqqq/MakeOPRGreatAgain/raw/master/5StarOneKey.user.js
@@ -8,11 +8,14 @@
 // @match        https://opr.ingress.com/recon
 // @grant        none
 // ==/UserScript==
-
 var buttons = [
 	{button:"Five Star", total:5, name:5, history:5, unique:5, location:5, safety:5},
 	{button:"553355", total:5, name:5, history:3, unique:3, location:5, safety:5},
-	{button:"533355", total:5, name:3, history:3, unique:3, location:5, safety:5},
+        {button:"551155", total:5, name:5, history:1, unique:1, location:5, safety:5},
+        {button:"552255", total:5, name:5, history:2, unique:2, location:5, safety:5},
+        {button:"552155", total:5, name:5, history:2, unique:1, location:5, safety:5},
+        {button:"551255", total:5, name:5, history:1, unique:2, location:5, safety:5},
+	{button:"551355", total:5, name:5, history:1, unique:3, location:5, safety:5},
 ];
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -32,7 +35,13 @@ function rate_portal(total, name, history, unique, location, safety) {
 
 function add_button() {
     var button_region = document.getElementById("submitDiv");
+    var i = 0;
     buttons.forEach(function(button_data) {
+        ++i;
+       if (i%4==0) {
+           var br = document.createElement("br");
+           button_region.appendChild(br);
+       }
         var button = document.createElement("button");
         var textnode = document.createTextNode(button_data["button"]);
         button.className = "button big-submit-button";
@@ -46,3 +55,4 @@ function add_button() {
 (function() {
     add_button();
 })();
+
